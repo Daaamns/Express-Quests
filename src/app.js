7119718cea9664360f1fea34a467c32de1f1b7b7
Adam HEMAMOU <h-mamou@hotmail.fr>
2023-11-28 11:49:27 +0100

    done quest 1.2

--- src/app.js
+++ src/app.js
@@ -4,12 +4,16 @@ const port = process.env.APP_PORT;
 
 const app = express();
 
+app.use(express.json());
+
 const movieControllers = require("./controllers/movieControllers");
 const userControllers = require("./controllers/userControllers");
 
+app.post("/api/movies", movieControllers.postMovie);
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
 app.get("/api/users", userControllers.getUsers);
 app.get("/api/users/:id", userControllers.getUserById);
+app.post("/api/users", userControllers.postUser);
 
 module.exports = app;
--- src/controllers/movieControllers.js
+++ src/controllers/movieControllers.js
@@ -27,17 +27,34 @@ const getMovieById = (req, res) => {
     .catch((err) => {
       console.error(err);
     });
-
-  // const movie = movies.find((movie) => movie.id === id);
-
-  // if (movie != null) {
-  //   res.json(movie);
-  // } else {
-  //   res.status(404).send("Not Found");
-  // }
 };
+
+const postMovie = (req, res) => {
+  const { title, director, year, color, duration } = req.body;
+
+  database
+    .query(
+      "INSERT INTO movies(title, director, year, color, duration) VALUE (?, ?, ?, ?, ?)",
+      [title, director, year, color, duration]
+    )
+    .then(([result]) => {
+      res.status(201).send({ id: result.insertId });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+// const movie = movies.find((movie) => movie.id === id);
+
+// if (movie != null) {
+//   res.json(movie);
+// } else {
+//   res.status(404).send("Not Found");
+// }
 
 module.exports = {
   getMovies,
   getMovieById,
+  postMovie,
 };
