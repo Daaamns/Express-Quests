34ef8f21ba452fcc700f0b16063e6528ba4bb141
Adam HEMAMOU <h-mamou@hotmail.fr>
2023-11-28 15:26:43 +0100

    quest 3 done!

--- tests/users.test.js
+++ tests/users.test.js
@@ -1,4 +1,5 @@
 const request = require("supertest");
+const crypto = require("node:crypto");
 
 const app = require("../src/app");
 const database = require("../database");
@@ -28,5 +29,55 @@ describe("GET /api/users/:id", () => {
     const response = await request(app).get("/api/users/0");
 
     expect(response.status).toEqual(404);
+  });
+});
+describe("POST /api/users", () => {
+  it("should return created user", async () => {
+    const newUser = {
+      firstname: "damien",
+      lastname: "jean",
+      email: `${crypto.randomUUID()}@wild.co`,
+      city: "marseillebb",
+      language: "deutch",
+    };
+
+    const response = await request(app).post("/api/users").send(newUser);
+
+    expect(response.headers["content-type"]).toMatch(/json/);
+    expect(response.status).toEqual(201);
+    expect(response.body).toHaveProperty("id");
+    expect(typeof response.body.id).toBe("number");
+
+    const [result] = await database.query(
+      "SELECT * FROM users WHERE id=?",
+      response.body.id
+    );
+
+    const [userInDatabase] = result;
+
+    expect(userInDatabase).toHaveProperty("id");
+    expect(userInDatabase).toHaveProperty("firstname");
+    expect(typeof userInDatabase.firstname).toBe("string");
+
+    expect(userInDatabase).toHaveProperty("lastname");
+    expect(typeof userInDatabase.lastname).toBe("string");
+
+    expect(userInDatabase).toHaveProperty("email");
+    expect(typeof userInDatabase.email).toBe("string");
+
+    expect(userInDatabase).toHaveProperty("city");
+    expect(typeof userInDatabase.city).toBe("string");
+
+    expect(userInDatabase).toHaveProperty("language");
+    expect(typeof userInDatabase.language).toBe("string");
+  });
+  it("should return an error", async () => {
+    const userWithMissingProps = { firstname: "Anna" };
+
+    const response = await request(app)
+      .post("/api/users")
+      .send(userWithMissingProps);
+
+    expect(response.status).toEqual(500);
   });
 });
