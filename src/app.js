520f52f3fe5e580de3e286b3b2cab81f9650d98a
Adam HEMAMOU <h-mamou@hotmail.fr>
2023-11-29 11:10:52 +0100

    quest 3.2 done!

--- src/app.js
+++ src/app.js
@@ -10,14 +10,24 @@ const movieControllers = require("./controllers/movieControllers");
 
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
-app.post("/api/movies", movieControllers.postMovie);
-app.put("/api/movies/:id", movieControllers.putMovie);
+// app.post("/api/movies", movieControllers.postMovie);
+// app.put("/api/movies/:id", movieControllers.putMovie);
 
 const userControllers = require("./controllers/userControllers");
 
 app.get("/api/users", userControllers.getUsers);
 app.get("/api/users/:id", userControllers.getUserById);
-app.post("/api/users", userControllers.postUser);
-app.put("/api/users/:id", userControllers.putUser);
+// app.post("/api/users", userControllers.postUser);
+// app.put("/api/users/:id", userControllers.putUser);
+
+const validateMovie = require("./middlewares/validateMovie");
+
+app.post("/api/movies", validateMovie, movieControllers.postMovie);
+app.put("/api/movies/:id", validateMovie, movieControllers.putMovie);
+
+const validateUser = require("./middlewares/validateUser");
+
+app.post("/api/users", validateUser, userControllers.postUser);
+app.put("/api/users/:id", validateUser, userControllers.putUser);
 
 module.exports = app;
